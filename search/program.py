39cd67f9b5d4c2e6023e8090f55671fcf7f01c59
Suhas <165161984+FLUTTERROJ@users.noreply.github.com>
2026-03-25 02:34:58 +0000

    Finished A* search, need to update the heuristic

--- search/program.py
+++ search/program.py
@@ -1,7 +1,7 @@
 # COMP30024 Artificial Intelligence, Semester 1 2026
 # Project Part A: Single Player Cascade
 
-from .core import CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction, PlayerColor
+from .core import BOARD_N, CellState, Coord, Direction, Action, MoveAction, EatAction, CascadeAction, PlayerColor
 from .utils import render_board
 import heapq
 
@@ -32,6 +32,61 @@ def heuristic(board: dict[Coord, CellState]) -> int:
             blue_stacks += 1
     return blue_stacks
 
+def move_stack(cord, direction, board):
+    '''
+    Moves the stack at the given coordinate to the next coordinate in the given direction, and returns the new board state.
+    '''
+
+    current_stack = board[cord]
+    target_cord = None
+    try:
+        target_cord = cord + direction
+    except ValueError:
+        pass
+    if target_cord in board:
+        board = move_stack(target_cord, direction, board)
+    board[target_cord] = CellState(color=current_stack.color, height=current_stack.height)
+    if cord in board:
+        del board[cord]
+    return board
+
+def apply_action(board, action):
+    '''
+    Applies the given action to the board and returns the new board state.
+    '''
+
+    board = board.copy()
+    if isinstance(action, MoveAction):
+        source_cord = action.coord
+        target_cord = source_cord + action.direction
+        if target_cord in board and board[target_cord].color == PlayerColor.RED:
+            old_height = board[target_cord].height
+            added_height = board[source_cord].height
+            board[target_cord] = CellState(color=PlayerColor.RED, height=old_height + added_height)
+        else:
+            board[target_cord] = CellState(color=PlayerColor.RED, height=board[source_cord].height)
+        del board[source_cord]
+    elif isinstance(action, EatAction):
+        source_cord = action.coord
+        target_cord = source_cord + action.direction
+        board[target_cord] = CellState(color=PlayerColor.RED, height=board[source_cord].height)
+        del board[source_cord]
+    elif isinstance(action, CascadeAction):
+        source_cord = action.coord
+        curr_cord = source_cord
+        height = board[source_cord].height
+        for i in range(0, height):
+            try:
+                target_cord = curr_cord + action.direction
+            except ValueError:
+                break
+            curr_cord = target_cord
+            if target_cord in board:
+                board = move_stack(target_cord, action.direction, board)
+            board[target_cord] = CellState(color=PlayerColor.RED, height=1)
+        del board[source_cord]
+    return board
+
 def get_legal_actions(board, cord):
     '''
     Checks for all the actions that are possible for the Red stack in that coordinate
@@ -39,9 +94,11 @@ def get_legal_actions(board, cord):
 
     actions = []
     for direction in Direction:
-        target_cord = cord + direction
-
-        if target_cord not in board:
+        try:
+            target_cord = cord + direction
+        except ValueError:
+            continue
+        if target_cord not in board or board[target_cord].color == PlayerColor.RED:
             actions.append(MoveAction(cord, direction))
         elif board[target_cord].color == PlayerColor.BLUE and board[target_cord].height <= board[cord].height:
             actions.append(EatAction(cord, direction))
@@ -50,32 +107,6 @@ def get_legal_actions(board, cord):
                 actions.append(CascadeAction(cord, direction))
     return actions
 
-def apply_action(board, action):
-    '''
-    Applies the given action to the board and returns the new board state.
-    '''
-    board = board.copy()
-    if isinstance(action, MoveAction):
-        source_cord = action.cord
-        target_cord = source_cord + action.direction
-        board[target_cord] = board[source_cord]
-        board[source_cord] = CellState()
-    elif isinstance(action, EatAction):
-        source_cord = action.cord
-        target_cord = source_cord + action.direction
-        board[target_cord] = CellState(color=PlayerColor.RED, height=board[source_cord].height)
-        board[source_cord] = CellState()
-    elif isinstance(action, CascadeAction):
-        source_cord = action.cord
-        i = 0
-        while board[source_cord].height > 0:
-            target_cord = source_cord + action.direction * i
-            board[target_cord] = CellState(color=PlayerColor.RED, height=1)
-            board[source_cord] = CellState(color=PlayerColor.RED, height=board[source_cord].height - 1)
-            i += 1
-        board[source_cord] = CellState()
-    return board
-    
 def search(
     board: dict[Coord, CellState]
 ) -> list[Action] | None:
@@ -103,40 +134,41 @@ def search(
     
     h_value = heuristic(board)
 
-    initial_node = (h_value, 0, board, [])
+    initial_node = (h_value,h_value, 0, 0, board, [])
     heapq.heappush(pq, initial_node)
 
-    state = frozenset(board.items())
-    reached = {state : 0}
-
+    board_state = frozenset(board.items())
+    reached = {board_state : 0}
+    count = 0
     while pq:
-        f_value, g_value, board, action_list = heapq.heappop(pq)  
+        f_value, h_value, _, g_value, curr_board, action_list = heapq.heappop(pq)  
         
-        if heuristic(board) == 0:
+        if heuristic(curr_board) == 0:
             return action_list
         
-        for cord, state in board.items():
+        for cord, state in curr_board.items():
             if state.color == PlayerColor.RED:
-                for action in get_legal_actions(board, cord):
-                    new_board = apply_action(board, action)
-                    state = frozenset(new_board.items())
-                    g_value += 1
-                    if state not in reached or g_value < reached[state]:
-                        reached[state] = g_value
-                        h_value = heuristic(new_board)
-                        f_value = g_value + h_value
-                        heapq.heappush(pq, (f_value, g_value, new_board, action_list + [action]))
+                for action in get_legal_actions(curr_board, cord):
+                    new_board = apply_action(curr_board, action)
+                    board_state = frozenset(new_board.items())
+                    new_g = g_value + 1
+                    new_h = heuristic(new_board)
+                    f_value = new_g + new_h
 
+                    if new_h == 0:
+                        return action_list + [action]
 
-
-
+                    if board_state not in reached or new_g < reached[board_state]:
+                        reached[board_state] = new_g
+                        count += 1
+                        heapq.heappush(pq, (f_value, new_h, count, new_g, new_board, action_list + [action]))
 
     # Here we're returning "hardcoded" actions as an example of the expected
     # output format. Of course, you should instead return the result of your
     # search algorithm. Remember: if no solution is possible for a given input,
     # return `None` instead of a list.
-    return action_list if action_list else [
-        MoveAction(Coord(3, 3), Direction.Down),
-        EatAction(Coord(4, 3), Direction.Down),
-        ]
-
+    return None
+'''
+MoveAction(Coord(3, 3), Direction.Down),
+EatAction(Coord(4, 3), Direction.Down),
+'''
